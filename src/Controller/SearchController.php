ea5a638e0b3e57c5dfddf5d8dbc49ed7ce1aa35d
RomeoDomiinguez <romeodominguezbocanegra@gmail.com>
2024-06-01 10:52:51 +0200

    new file:   assets/styles/buscador.css 	modified:   assets/styles/post.css 	modified:   composer.json 	modified:   composer.lock 	modified:   config/bundles.php 	modified:   config/routes.yaml 	new file:   src/Controller/SearchController.php 	modified:   symfony.lock 	modified:   templates/feed.html.twig 	modified:   templates/new_post.html.twig 	new file:   templates/results.html.twig

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
+use App\Entity\Post;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function search(Request $request, PostRepository $postRepository): Response
+    {
+        // Obtener parámetros de búsqueda del formulario
+        $searchTerm = $request->query->get('search', '');
+        $searchBy = $request->query->get('searchBy', 'all'); // 'all', 'title', 'category', 'author'
+
+        // QueryBuilder para buscar en los posts
+        $postsQuery = $postRepository->createQueryBuilder('p');
+
+        // Aplicar filtros según los parámetros de búsqueda
+        if (!empty($searchTerm)) {
+            if ($searchBy === 'title' || $searchBy === 'all') {
+                $postsQuery->andWhere('p.titulo LIKE :searchTerm')
+                    ->setParameter('searchTerm', '%' . $searchTerm . '%');
+            }
+            if ($searchBy === 'category' || $searchBy === 'all') {
+                $postsQuery->andWhere('p.categoria LIKE :searchTerm')
+                    ->setParameter('searchTerm', '%' . $searchTerm . '%');
+            }
+        }
+
+        // Obtener los resultados de la búsqueda
+        $posts = $postsQuery->orderBy('p.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('results.html.twig', [
+            'posts' => $posts,
+            'searchTerm' => $searchTerm,
+            'searchBy' => $searchBy,
+        ]);
+    }
+}
